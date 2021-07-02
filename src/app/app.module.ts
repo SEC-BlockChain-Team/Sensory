b9e955d94480ba3110927a593ac35969ae54df13
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-07-02 23:56:46 +0600

    created shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { DataComponent } from './data/data.component';
 import { EthereumComponent } from './data/ethereum/ethereum.component';
 import { VechainComponent } from './data/vechain/vechain.component';
 import { IotaComponent } from './data/iota/iota.component';
 import { SensorsComponent } from './sensors/sensors.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 // Material Imports
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
