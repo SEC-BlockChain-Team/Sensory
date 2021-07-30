569c8059c928e26f42a64c28a5cce40cb507872f
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-07-31 00:57:02 +0600

    thorify implementation for vechain chain

--- src/app/data/vechain/vechain.component.ts
+++ src/app/data/vechain/vechain.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {VechainService} from "./vechain.service";
 
 @Component({
   selector: 'app-data-vechain',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vechain.component.scss']
 })
 export class VechainComponent implements OnInit {
+  sensorData: number[];
+  dataCounter: number;
 
-  constructor() { }
+  transactionDatas: any;
+  transactionData: any;
+
+  constructor(
+    private vechainService: VechainService
+  ) {
+  }
 
   ngOnInit(): void {
   }
+
+  async sendTransaction() {
+    console.log("started")
+    this.transactionData = await this.vechainService.createData('0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    console.log("ended")
+  }
+
+  async getData() {
+    console.log("started")
+    this.sensorData = await this.vechainService.getLatestData();
+    console.log("ended")
+  }
+
+  async getNthData() {
+    console.log("started")
+    this.sensorData = await this.vechainService.getData(0);
+    console.log("ended")
+  }
+
+  // async getDataCounter() {
+  //   this.dataCounter = await this.vechainService.getDataCounter();
+  // }
 
 }
