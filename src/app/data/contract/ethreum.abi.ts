3ec2163a9600727e359165a26bbe402d0f0a17b1
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-07-31 00:56:42 +0600

    web3 implementation for ethereum chain

---
+++ src/app/data/contract/ethreum.abi.ts
@@ -0,0 +1,102 @@
+export const ETHEREUM_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "_datas",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "createData",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "stateMutability": "payable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "dataCounter",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "dataList",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_dataId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getData",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getDataCounter",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getLatestData",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
--- src/app/data/ethereum/ethereum.component.ts
+++ src/app/data/ethereum/ethereum.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {EthereumService} from "./ethereum.service";
 
 @Component({
@@ -8,14 +8,40 @@ import {EthereumService} from "./ethereum.service";
 })
 export class EthereumComponent implements OnInit {
 
-  blockData: any;
+  sensorData: number[];
+  dataCounter: number;
+
+  transactionDatas: any;
+  transactionData: any;
 
   constructor(
     private ethService: EthereumService
-  ) { }
-
-  ngOnInit(): void {
-    this.blockData = this.ethService.getLatestBlock();
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getDataCounter();
+  }
+
+  async sendTransaction() {
+    console.log("started")
+    this.transactionData = await this.ethService.createData([5,6,7,8,9])
+    console.log("ended")
+  }
+
+  async getData() {
+    console.log("started")
+    this.sensorData = await this.ethService.getLatestData();
+    console.log("ended")
+  }
+
+  async getNthData() {
+    console.log("started")
+    this.sensorData = await this.ethService.getData(1);
+    console.log("ended")
+  }
+
+  async getDataCounter() {
+    this.dataCounter = await this.ethService.getDataCounter();
+  }
 }
